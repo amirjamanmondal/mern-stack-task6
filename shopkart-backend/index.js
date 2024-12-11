dcfac22a120b4f89a2af229d0ce8bc2594faf9b2
Amir Jaman Mondal <amirjamanmondal@gmail.com>
2024-12-11 21:20:04 +0530

    first commit for task 6

---
+++ shopkart-backend/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 4 * 60 * 60 * 1000 },
+  })
+);
+
+
+
+app.listen(port, function () {
+  console.log(`server is running on port ${port}`);
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(function (res) {
+    console.log(`database connected to ${res.connection.host}`);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
