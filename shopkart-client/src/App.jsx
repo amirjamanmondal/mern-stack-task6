d5534d0aa9cd73aab2c7a375536952e8a1aa395b
Amir Jaman Mondal <amirjamanmondal@gmail.com>
2024-12-16 08:23:20 +0530

    commit

--- shopkart-client/src/App.jsx
+++ shopkart-client/src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 import Login from "./components/auth/Login";
 import UploadImage from "./components/common/UploadImage";
 import AddProduct from "./components/forms/AddProduct";
@@ -9,8 +12,26 @@ import Navbar from "./components/header/Navbar";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const res = await axios.get(`http://localhost:8000/user/getProduct`, {
+        withCredentials: true,
+      });
+      const data = res.data;
+      setProducts(data.products);
+      toast(data.message);
+    }
+  }, []);
+
+  if (!products) {
+    return( <div>Loading...</div>)
+  }
+
   return (
     <div className="min-h-screen w-full flex flex-col items-center justify-start bg-gray-100">
+      <Toaster />
       <Navbar />
       <div className="w-full h-fit flex items-center justify-evenly flex-wrap gap-4 row-span-3 my-4">
         <SliderImage />
